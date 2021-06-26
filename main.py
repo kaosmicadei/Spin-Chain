8b5eb4898a1b3dd2ccc7af3b4d84130692dd7e39
Kaonan Micadei <k.micadei@gmail.com>
2021-06-26 12:36:25 +0200

    Initial state preparation

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import numpy as np
 from generators import OpGenerator
-from hamiltonian import Hamiltonian
+from hamiltonian import XYModel
 
+# Spin chain initialisation
 N = 3
 op = OpGenerator(N)
-hamiltonian = Hamiltonian(op)
-print(hamiltonian.H0, '\n')
-print(hamiltonian.Hint, '\n')
+hamiltonian = XYModel(op)
+
+# State preparation
+beta = 0.1
+thermal_chain = np.exp(-beta * np.diag(hamiltonian.H0))
+partition_func = np.sum(thermal_chain)
+thermal_chain = np.diag(thermal_chain / partition_func)
+
+# State to be transmited through the chain
+message_state = op.z_projector[0][0]
+
+# Initial state of the whole chain
+init_state = np.matmul(message_state, thermal_chain)
+print(init_state)
