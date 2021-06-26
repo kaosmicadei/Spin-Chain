Fix initial state normalisation

hamiltonian.py
@@ -1,16 +1,18 @@
-from generators import OpGenerator
+from generators import HalfSpinOperator
 import numpy as np
 
 class XYModel:
     H0 = 0
     Hint = 0
 
-    def __init__(self, op: OpGenerator):
+    def __init__(self, op: HalfSpinOperator, coupling_constant, omega):
         self.dimension = op.dimension
 
         for i in range(1, op.dimension):
             self.H0 += op.sigma['z'][i]
+        self.H0 *= omega
 
         for i in range(op.dimension-1):
             self.Hint += np.matmul(op.sigma['x'][i], op.sigma['x'][i+1]) + \
                          np.real(np.matmul(op.sigma['y'][i], op.sigma['y'][i+1]))
+        self.Hint *= coupling_constant

main.py
@@ -1,21 +1,26 @@
 import numpy as np
-from generators import OpGenerator
+from generators import HalfSpinOperator
 from hamiltonian import XYModel
 
 # Spin chain initialisation
 N = 3
-op = OpGenerator(N)
-hamiltonian = XYModel(op)
+op = HalfSpinOperator(N)
+hamiltonian = XYModel(op, coupling_constant=-0.5, omega=0.5)
 
-# State preparation
-beta = 0.1
+
+# =================
+# STATE PREPARATION
+# =================
+
+# Thermal chain
+beta = 1
 thermal_chain = np.exp(-beta * np.diag(hamiltonian.H0))
-partition_func = np.sum(thermal_chain)
-thermal_chain = np.diag(thermal_chain / partition_func)
+partition_function = np.sum(thermal_chain)
+thermal_chain = 2.0 * np.diag(thermal_chain / partition_function)
 
 # State to be transmited through the chain
 message_state = op.z_projector[0][0]
 
-# Initial state of the whole chain
+# Whole initial system
 init_state = np.matmul(message_state, thermal_chain)
 print(init_state)