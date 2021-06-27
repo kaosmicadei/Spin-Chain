ff4f045c17601c2b34ef86eb14ec91d1a3036436
Kaonan Micadei <k.micadei@gmail.com>
2021-06-27 10:00:19 +0200

    Chain evolution

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import numpy as np
 from chain import Chain
 
-zero = np.array([[.8, 0.],
-                 [0., .2]])
+zero = np.array([[.9, 0.],
+                 [0., .1]])
 
 # Initialisation
 N = 5
 dt = 0.001
-chain = Chain(N, coupling_constant=-0.5, omega=0.5, message_state=zero, dt=dt)
+chain = Chain(N, coupling_constant=0.5, omega=0.5, message_state=zero, dt=dt)
 
 # Evolution
 time_interval = int(np.pi / dt)
@@ -18,6 +18,6 @@ evolution = [ np.array([chain.bloch_vector(i) for i in range(chain.dimension)]) 
 for t in range(time_interval):
     chain.next()
     if t % step_length:
-        evolution.append(np.array([chain.bloch_vector(i) for i in range(chain.dimension)]))
+        evolution.append(np.array([ chain.bloch_vector(i) for i in range(chain.dimension) ]))
 evolution = np.array(evolution)
-print(evolution)
+print(evolution[:,:,2])
