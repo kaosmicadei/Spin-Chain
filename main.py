a8d8991b2a71db7a3fafe2bfa4494e27fa366df7
Kaonan Micadei <k.micadei@gmail.com>
2021-07-23 20:57:57 +0200

    Testing new value of beta

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ rho = np.array([[.7 , -.1j],
 # Initialisation
 N = 5
 dt = 0.001
-chain = Chain(N, coupling_constant=0.5, omega=0.5, message_state=rho, dt=dt)
+chain = Chain(N, coupling_constant=0.5, omega=0.5, beta=0.1, message_state=rho, dt=dt)
 
 # Evolution
 time_interval = int(np.pi / dt)
 timesteps = 20
-step_length = int(np.pi / dt / timesteps)
+step_length = time_interval // timesteps
 
 evolution = [ np.array([chain.bloch_vector(i) for i in range(chain.dimension)]) ]
 for t in range(time_interval):
